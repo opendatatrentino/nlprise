c62887236dcdbf48e91e0d92e41ef84854e5981e
Alberto Zanella <fakemail@fakemail.it>
2013-07-04 18:21:59 +0200

    added date identifier

---
+++ nlprise/src/main/java/eu/trentorise/nlprise/identifiers/date/Response.java
@@ -0,0 +1,43 @@
+package eu.trentorise.nlprise.identifiers.date;
+
+import java.util.HashMap;
+
+/**
+ * Represent the response from the DateParser.isDate()
+ * @author azanella
+ * Last modified by azanella On Jul 4, 2013
+ */
+public class Response {
+	/**
+	 * Contains parser used to identify date. INTERNAL is the parser.ItalianDateParser
+	 * @author azanella
+	 *
+	 */
+	public enum DateParser { INTERNAL, JCHRONIC, POJAVA;}
+		
+	private boolean result;
+	private HashMap<DateParser, Boolean> singleParserReturn;
+	
+	/**
+	 * 
+	 * @return if the string passed was identified as a Date by at least one parser
+	 */
+	public boolean getResult() {
+		return result;
+	}
+	/**
+	 * 
+	 * @return an HashMap containing the parser as the key and the result of the evaluation (boolean) as value
+	 */
+	public HashMap<DateParser, Boolean> getSingleParserReturn() {
+		return singleParserReturn;
+	}
+	protected void setResult(boolean result) {
+		this.result = result;
+	}
+	protected void setSingleParserReturn(
+			HashMap<DateParser, Boolean> singleParserReturn) {
+		this.singleParserReturn = singleParserReturn;
+	}
+	
+}
